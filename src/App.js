37c2582c3bfa813d26688b76da87bae59a51c520
Alditya Dwinata <dwinatalditya16@gmail.com>
2023-01-03 11:16:04 +0700

    add crud function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { TodoList } from './component/TodoList'
+import { TodoAdd } from './component/TodoAdd'
+import {
+  getTodoFromLocalStorage,
+  saveTodoToLocalStorage,
+} from "./utils/LocalStorage";
 
 function App() {
+  const [todos, setTodos] = useState(getTodoFromLocalStorage("todos") || []);
+
+  const addTodo = (value) => {
+    const newTodos = [...todos, { text: value }];
+    setTodos(newTodos);
+    saveTodoToLocalStorage("todos", newTodos)
+  };
+
+  const toggleTodo = (ind) => {
+    const newTodos = [...todos];
+    newTodos[ind].isCompleted = !newTodos[ind].isCompleted;
+    setTodos(newTodos);
+    saveTodoToLocalStorage("todos", newTodos);
+  };
+
+  const deleteTodo = (ind) => {
+    const newTodos = [...todos];
+    newTodos.splice(ind, 1);
+    setTodos(newTodos);
+    saveTodoToLocalStorage("todos", newTodos)
+  };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ALDIT TODO-APP</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+      <TodoAdd addTodo={addTodo} />
     </div>
   );
 }
